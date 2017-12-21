8b70172f84b53189444ca8610b3ea14c6fbf49b5
Sebastien Diaz <sebastien.diaz@misys.com>
2017-12-21 11:43:35 +0100

    Add more and more jwt feature

--- nodejs-server/tools/Config.js
+++ nodejs-server/tools/Config.js
@@ -1,6 +1,9 @@
 
 exports.getHelmPath = function() {
         var path = "charts";
+        if(process.env['DIMIDIUM_HELM_PATH']){
+          path=process.env['DIMIDIUM_HELM_PATH'];
+        }
         if(process.argv.indexOf("--helmpath") != -1){ //does our flag exist?
           path = process.argv[process.argv.indexOf("--helmpath") + 1]; //grab the next item
         }
@@ -11,6 +14,9 @@ exports.getHelmPath = function() {
 
 exports.getMongoDbUrl = function() {
     var path = "mongodb://localhost:27017";
+    if(process.env['DIMIDIUM_MONGODB_URL']){
+      path=process.env['DIMIDIUM_MONGODB_URL'];
+    }
     if(process.argv.indexOf("--mongodburl") != -1){ //does our flag exist?
       path = process.argv[process.argv.indexOf("--mongodburl") + 1]; //grab the next item
     }
@@ -20,6 +26,9 @@ exports.getMongoDbUrl = function() {
 };
 exports.getMongoDbName = function() {
     var path = "dimidium";
+    if(process.env['DIMIDIUM_MONGODB_NAME']){
+      path=process.env['DIMIDIUM_MONGODB_NAME'];
+    }
     if(process.argv.indexOf("--mongodbname") != -1){ //does our flag exist?
       path = process.argv[process.argv.indexOf("--mongodbname") + 1]; //grab the next item
     }
@@ -30,6 +39,9 @@ exports.getMongoDbName = function() {
 
 exports.getRepoName = function() {
     var path = "dimidium";
+    if(process.env['DIMIDIUM_REPO_NAME']){
+      path=process.env['DIMIDIUM_REPO_NAME'];
+    }
     if(process.argv.indexOf("--reponame") != -1){ //does our flag exist?
       path = process.argv[process.argv.indexOf("--reponame") + 1]; //grab the next item
     }
@@ -40,6 +52,9 @@ exports.getRepoName = function() {
 
 exports.getRepoUrl = function() {
     var path = "http://localhost:8080/v1/service/repo";
+    if(process.env['DIMIDIUM_REPO_URL']){
+      path=process.env['DIMIDIUM_REPO_URL'];
+    }
     if(process.argv.indexOf("--reponame") != -1){ //does our flag exist?
       path = process.argv[process.argv.indexOf("--reponame") + 1]; //grab the next item
     }
